b43320ab203a4d95694f77dbd664528b1c460363
Ibrahim Sel <ibrahim.sel@eteration.com>
2024-11-15 12:29:28 +0300

    add racecar ns as env

--- launch/gap.launch.py
+++ launch/gap.launch.py
@@ -1,6 +1,8 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 import os
 import socket
 
@@ -11,12 +13,21 @@ def generate_launch_description():
     config_file = os.path.join(
         get_package_share_directory("reactive_gap_follower"), "config", "params.yaml"
     )
+    racecar_namespace_arg = DeclareLaunchArgument(
+        "racecar_namespace",
+        default_value=f"{os.getenv('racecar_ns', 'racecar')}",
+        description="Racecar Namespace",
+    )
 
     gap_follower_node = Node(
         package="reactive_gap_follower",
         executable="cass_gap_follower",
         name=f"cass_gap_follower_{hostname}",
-        parameters=[config_file],
+        parameters=[
+            config_file,
+            {"racecar_namespace": LaunchConfiguration("racecar_namespace")},
+        ],
     )
     ld.add_action(gap_follower_node)
+    ld.add_action(racecar_namespace_arg)
     return ld
